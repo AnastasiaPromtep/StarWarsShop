2bf52c40fce2a2e30c3e2d5b30460c3975c547a6
Anastasia Prom tep <apromtep@gmail.com>
2021-10-09 15:15:57 +0200

    Add weapon list component

---
+++ src/app/model/weapon.model.ts
@@ -0,0 +1,9 @@
+export class WeaponModel {
+
+    constructor(
+        public name: string,
+        public shortName: string,
+        public damage: number,
+        public critical: number
+    ) {}
+}
